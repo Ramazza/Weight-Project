022eeca17582f265e64ed4a4b26b7dc3d00c572b
PecorinaAmaga01! <01ramazza@gmail.com>
2024-05-22 14:14:13 -0300

    Add handleToggle

--- src/contexts/userContexts.tsx
+++ src/contexts/userContexts.tsx
@@ -1,11 +1,9 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import api from "../api";
 
 export const userContext = createContext({} as any);
 
 export const UserStorage = ({ children }: any) => {
-
-
 
     const handleCreateAccount = (name: string, email: string, password: string, password2: string) => {
 
@@ -17,9 +15,30 @@ export const UserStorage = ({ children }: any) => {
 
     }
 
+    const [isToggled, setIsToggled] = useState(false);
+    const [animationState, setAnimationState] = useState(0)
+
+    const handleToggle = () => {
+        if(animationState === 0) {
+            setAnimationState(1);
+            setIsToggled(!isToggled);
+        }
+        if(animationState === 1) {
+            setAnimationState(2);
+            setIsToggled(!isToggled);
+        }
+        if(animationState === 2) {
+            setAnimationState(1);
+            setIsToggled(!isToggled);
+        }
+    }
+
     return(
         <userContext.Provider value={{
             handleCreateAccount,
+            handleToggle,
+            isToggled,
+            animationState,
         }}>
             {children}
         </userContext.Provider>
