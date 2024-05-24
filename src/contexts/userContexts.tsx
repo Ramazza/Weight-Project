406d9ee5b96b6963ef565264e9717a4767c10dff
PecorinaAmaga01! <01ramazza@gmail.com>
2024-05-24 17:58:37 -0300

    Add handleLogin

--- src/contexts/userContexts.tsx
+++ src/contexts/userContexts.tsx
@@ -5,10 +5,24 @@ export const userContext = createContext({} as any);
 
 export const UserStorage = ({ children }: any) => {
 
+    const navigateTo = (path: any) => {
+		window.location.href = path;
+	};
+
+    const handleLogin = (email: string, password: string) => {
+        api.post('/user/sign-in', {email, password}).then(({ data}) => {
+            console.log('Login feito com sucesso!');
+            navigateTo('/home');
+        }).catch((error) => {
+            console.log('Não foi possível fazer o login', error);
+        })
+    }
+
     const handleCreateAccount = (name: string, email: string, password: string, password2: string) => {
 
         api.post('/user/sign-up', {name, email, password}).then(() => {
             console.log('Usuário criado com sucesso!');
+            handleLogin(email, password);
         }).catch((error) => {
             console.log('Não foi possível criar o usuário', error)
         })
@@ -37,6 +51,7 @@ export const UserStorage = ({ children }: any) => {
         <userContext.Provider value={{
             handleCreateAccount,
             handleToggle,
+            handleLogin,
             isToggled,
             animationState,
         }}>
