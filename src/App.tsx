18ee9085720ced36fd59f3e63c22599085c94603
PecorinaAmaga01! <01ramazza@gmail.com>
2024-05-27 21:38:29 -0300

    Add logic to handle routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './pages/home';
 import SignUp from './pages/sign-up';
+import { useEffect } from 'react';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if(token) {
+      navigate('/home');
+    } else {
+      navigate('/');
+    }
+  },[navigate]);
+
   return (
-    <SignUp />
+      <Routes>
+        <Route path='/' element={<SignUp />}/>
+        <Route path='/home' element={<Home />}/>
+      </Routes>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return(
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
