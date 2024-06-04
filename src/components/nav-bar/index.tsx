842905dea294b1ede090eecb0b1a17be77e876d2
PecorinaAmaga01! <01ramazza@gmail.com>
2024-06-04 09:54:59 -0300

    Add logout button

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,15 +1,35 @@
-import { Container, Logo, ProfileImage, Title } from "./styles";
+import { Container, Logo, ProfileImage, Title, PorfileOptionsContainer, Logout } from "./styles";
 import LogoImg from '../../assets/logo.png';
 import UserImg from '../../assets/user.png';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
 
+    const [isClicked, setIsClicked] = useState(false);
+
+    const navigate = useNavigate();
+
+    const logOff = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    }
+
     return(
+        <>
         <Container>
             <Logo src={LogoImg} alt="Logo"/>
             <Title>My Fit Journey</Title>
-            <ProfileImage src={UserImg} alt="Profile"/>
+            <ProfileImage src={UserImg} alt="Profile" onMouseDown={() => setIsClicked(!isClicked)}/>
         </Container>
+        { isClicked ? 
+            <PorfileOptionsContainer>
+                <Logout onMouseDown={() => logOff()}>Sair</Logout>
+            </PorfileOptionsContainer>   
+            :
+            '' 
+        }
+        </>
     );
 }
 
--- src/components/nav-bar/styles.ts
+++ src/components/nav-bar/styles.ts
@@ -35,3 +35,31 @@ export const ProfileImage = styled.img`
     }
 `;
 
+export const PorfileOptionsContainer = styled.div`
+    position: absolute;
+    width: 4.5rem;
+    height: 3rem;
+    right: 0rem;
+    top: 3.15rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #F2F2F2;
+    border-bottom: 2px solid #0051A3;
+    border-left: 2px solid #0051A3;
+`;
+
+export const Logout = styled.button`
+    width: 70%;
+    height: 50%;
+    background-color: #0051A3;
+    color: white;
+    border: none;
+    outline: none;
+    font-size: 1.1em;
+    font-weight: 600;
+
+    &:hover {
+        transform: scale(1.05);
+    }
+`;
